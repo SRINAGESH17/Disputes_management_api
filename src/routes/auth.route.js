f07f5e063f739e847326a471079b5bbe8458381a
AbdulRazzak157 <abdulrazzak@skygoalnext.com>
2025-06-30 17:04:35 +0530

    add(forgot-password) : added apis to verify OTP and reset Password

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,8 +4,20 @@ import sentVerifyEmailOTP from '../controllers/zepto/sent-email-otp.controller.j
 import verifyEmailOTP from '../controllers/zepto/verify-email-otp.controller.js';
 import verifyMobileOTP from '../controllers/msg91/verify-mobile-otp.controller.js';
 import sentMobileOTP from '../controllers/msg91/sent-mobile-otp.controller.js';
+import sentForgotPasswordEmailOTP from '../controllers/zepto/sent-forgot-password-otp.controller.js';
+import verifyForgotPasswordEmailOTP from '../controllers/zepto/verify-forgot-password-otp.controller.js';
+import resetUserPassword from '../controllers/forgot-password.controller.js';
 
 const router = express.Router();
+
+
+// 1.  EMAIL OTP
+// 2.  Mobile Number OTP
+// 3.  Merchant Registration
+// 4.  Forgot Password
+
+
+
 
 // ********************************************** EMAIL OTP ******************************************
 
@@ -40,6 +52,27 @@ router.post('/verify-mobile-number-otp/:mobileNumber', verifyMobileOTP);
 // @desc        : Create Merchant Account
 // @access      : Public to Merchants
 router.post('/register', registerMerchant);
+
+
+// ******************************************** Forgot Password **************************************
+
+// Sent Email OTP
+// @route       : POST /auth/forgot-password/sent-email-otp/:email
+// @desc        : Sent Forgot password email OTP
+// @access      : Public
+router.post('/forgot-password/sent-email-otp/:email', sentForgotPasswordEmailOTP);
+
+// Verify Email OTP
+// @route       : POST /auth/forgot-password/verify-email-otp/:email
+// @desc        : Verify Forgot password email OTP
+// @access      : Public
+router.post('/forgot-password/verify-email-otp/:email', verifyForgotPasswordEmailOTP);
+
+// Reset Password
+// @route       : POST /auth/forgot-password/reset-password/:email
+// @desc        : Reset Password
+// @access      : Public
+router.post('/forgot-password/reset-password/:email', resetUserPassword);
 
 
 export default router;
