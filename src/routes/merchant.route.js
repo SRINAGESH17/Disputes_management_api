0c20458c03facbe59c6ad85ba5b7e68e92564fa8
jellaharikrishna <jellaharikrishna@skygoalnext.com>
2025-07-01 17:19:46 +0530

    refactor(api route): gateways controller added

--- src/routes/merchant.route.js
+++ src/routes/merchant.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import AddMerchantStaff from '../controllers/merchant/staff/add-staff.controller.js';
-import {addGateway, fetchGateways} from '../controllers/merchant/integration/gateway.controller.js';
+import gatewayController from '../controllers/merchant/integration/gateway.controller.js';
 import { verifyMerchant } from '../middlewares/auth.middleware.js';
 
 const router = express.Router();
@@ -20,18 +20,18 @@ router.post(
 );
 
 
-// ****************************Gateway*****************************/
+// **************************** Gateway oR Integration *****************************/
 // 2. Add Gateway
 // @route : POST /api/v2/merchant/integration/gateway
 // @desc  : Add Gateway for Merchant
-// @access: Public
-router.post('/integration/gateway', verifyMerchant, addGateway)
+// @access: Private to Merchant
+router.post('/integration/gateway', verifyMerchant, gatewayController.addGateway)
 
 // 3. fetch Gateways
 // @route : GET /api/v2/merchant/integration/gateway
 // @desc  : Fetch Gateway for Merchant
-// @access: Public
-router.get('/integration/gateway', verifyMerchant, fetchGateways);
+// @access: Private to Merchant
+router.get('/integration/gateway', verifyMerchant, gatewayController.fetchGateways);
 
 
 
