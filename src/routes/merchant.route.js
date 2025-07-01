34d796a4b99b86cabcf79a8894f6216bc7e4e410
Tharun1219 <tharun@skygoalnext.com>
2025-07-01 16:23:51 +0530

    feat(staff): staff feed apis added

--- src/routes/merchant.route.js
+++ src/routes/merchant.route.js
@@ -4,6 +4,9 @@ import { verifyMerchant } from "../middlewares/auth.middleware.js";
 import welcomeDashboard from "../controllers/merchant/dashboard/welcome-screen.controller.js";
 import updateStaffStatus from "../controllers/merchant/staff/update-staff-status.controller.js";
 import staffStatusCards from "../controllers/merchant/staff/staff-status-cards.controller.js";
+import getAllStaff from "../controllers/merchant/staff/get-all-staff.controller.js";
+import getStaff from "../controllers/merchant/staff/get-staff.controller.js";
+import merchantProfile from "../controllers/merchant/dashboard/merchant-profile.controller.js";
 
 const router = express.Router();
 
@@ -14,6 +17,13 @@ const router = express.Router();
 // @access : Private to merchant Only !
 //
 router.get("/dashboard/welcome", verifyMerchant, welcomeDashboard);
+
+// 2.Merchant Profile page
+// @route  :GET /api/v2/merchant/profile
+// @desc   : Displaying the Merchant Profile
+// @access : Private to Merchant Only
+
+router.get("/profile", verifyMerchant, merchantProfile);
 
 // *********************************** Staff Routes
 
@@ -27,22 +37,32 @@ router.post(
   AddMerchantStaff // Add Merchant Staff Controller
 );
 
-
-
 // 2. Change Staff Status
 // @route  : PUT /api/v2/merchant/staff/:staffId
 // @desc   : Changing Staff Status
 // @access : Private to Merchant Only
 
-router.put('/staff/:staffId', verifyMerchant, updateStaffStatus);
-
+router.put("/staff/status/:staffId", verifyMerchant, updateStaffStatus);
 
 // 3 . Fetch Status Cards
 // @route  : GET / api/v2/merchant/staff/status
 // @desc   : Fetching all the Status Count
 // @access : Private to Merchant Only
 
-router.get('/staff/status', verifyMerchant, staffStatusCards);
+router.get("/staff/status", verifyMerchant, staffStatusCards);
 
+// 4. Fetch all Staff and Filtering
+// @route    : GET/api/v2/merchant/staff/all
+// @desc     : Fetching all the Staff and Filtering
+// @access   : Private to Merchant Only
+
+router.get("/staff/all", verifyMerchant, getAllStaff);
+
+// 5 . Fetching the Staff
+// @route   : GET /api/v2/merchant/staff/:staffId
+// @desc    : Fetching the Staff Based on Staff Id
+// @access  : Private to Merchant Only
+
+router.get("/staff/:staffId", verifyMerchant, getStaff);
 
 export default router;
