5218b273132bce1d0ce0dea4d5ce4fcb83ba05fb
jellaharikrishna <jellaharikrishna@skygoalnext.com>
2025-07-10 18:05:00 +0530

    fix-integration

--- src/services/merchant/merchant-register.service.js
+++ src/services/merchant/merchant-register.service.js
@@ -62,13 +62,13 @@ const merchantRegisterService = async (data) => {
     const [
       userEmailRecord,
       userMobileRecord,
-      isEmailVerified,
-      isMobileNumberVerified
+      // isEmailVerified,
+      // isMobileNumberVerified
     ] = await Promise.all([
       FirebaseCheckEmailExistOrNot(email),
       FirebaseCheckPhoneExistOrNot(mobileNumber),
-      OTP.findOne({ where: emailPayload, attributes: ['verificationValue'], raw: true }),
-      OTP.findOne({ where: mobilePayload, attributes: ['verificationValue'], raw: true })
+      // OTP.findOne({ where: emailPayload, attributes: ['verificationValue'], raw: true }),
+      // OTP.findOne({ where: mobilePayload, attributes: ['verificationValue'], raw: true })
     ]);
 
     // 1.1 : Check Email is Exist or not
