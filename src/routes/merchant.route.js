ce7eb437261bbef14fccce1388d5a4b03e5c095c
AbdulRazzak157 <abdulrazzak@skygoalnext.com>
2025-07-11 11:46:40 +0530

    Add(disputes): Added merchant dispute apis

--- src/routes/merchant.route.js
+++ src/routes/merchant.route.js
@@ -9,6 +9,7 @@ import welcomeDashboard from "../controllers/merchant/dashboard/welcome-screen.c
 // import getStaff from "../controllers/merchant/staff/get-staff.controller.js";
 import merchantProfile from "../controllers/merchant/dashboard/merchant-profile.controller.js";
 import businessController from "../controllers/merchant/kyb/business.controller.js";
+import merchantDisputeController from "../controllers/merchant/merchant-disputes.controller.js";
 
 const router = express.Router();
 
@@ -96,6 +97,28 @@ router.post(
   '/kyb/gst',
   verifyMerchant,
   businessController.addNewBusinessAccount
+);
+
+// *********************************************** Merchant Disputes ****************************************/
+
+// 1.Fetch Internal Dispute States
+// @route  : GET /api/v2/merchant/states
+// @desc   : Fetch Internal Disputes States
+// @access : Private to merchant Only !
+router.get(
+  '/states',
+  verifyMerchant,
+  merchantDisputeController.getDisputeStates
+);
+
+// 2.Fetch Disputes List With Filters
+// @route  : GET /api/v2/disputes/list
+// @desc   : Fetch Disputes List with filters
+// @access : Private to merchant Only !
+router.get(
+  '/list',
+  verifyMerchant,
+  merchantDisputeController.getDisputesList
 );
 
 
